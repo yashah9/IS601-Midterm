e9460de97f82b085af59b83817e7de640f2839b8
yashah9 <yashshah0910@gmail.com>
2025-03-08 18:31:52 -0500

    Basic Setup

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This module serves as the entry point for the application.
+"""
+
+from app import App
+
+# This forces the program to start when you run it from the command line.
+if __name__ == "__main__":
+    App().start()  # Instantiate an instance of App
